89fda3ddd80041ef7abcb67bc222251a52de2615
wagner lemos <wagner.lemos94@hotmail.com>
2022-01-14 15:14:20 -0300

    Implemeta configuraçao de requiscao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [
