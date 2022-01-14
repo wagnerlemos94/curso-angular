5d25d19dddc48613fc8d7f0b9a082e7b322e0480
wagner lemos <wagner.lemos94@hotmail.com>
2022-01-14 14:37:21 -0300

    Implementa model de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,9 +1,10 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category} from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService{
     createDb(){
-        const categories = [
-            { id:1, name: "Moradia", description:"Pagamentos de Contas de Casa"},
+        const categories: Category[] = [
+            { id:1,  name: "Moradia", description:"Pagamentos de Contas de Casa"},
             { id:2, name: "Saúde", description:"Plano de Saúde e Remédios"},
             { id:3, name: "Lazer", description:"Conema, parques, praia, etc"},
             { id:4, name: "Salário", description:"Recebimento de Saçário"},
