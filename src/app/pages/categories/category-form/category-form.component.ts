305c1df9dc43c959a461c1849636205b2d19716f
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2022-01-17 16:45:58 -0300

    Implementa metodos de crate e update

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -40,6 +40,15 @@ export class CategoryFormComponent implements OnInit,AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm(){
+    this.submittingForm = true;
+    if(this.currentAction == "new"){
+      this.createCategory();
+    }else{
+      this.updateCategory();
+    }
+  }
+
   private setCurrentAction(){
     if(this.route.snapshot.url[0].path == "new"){
       this.currentAction = "new";
@@ -77,6 +86,44 @@ export class CategoryFormComponent implements OnInit,AfterContentChecked {
     }else{
       const CategoryName = this.category.name || ""
       this.pageTitle = "Editando Categoria: " + CategoryName;
+    }
+  }
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.create(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso!");
+    
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    );
+
+  }
+
+  private actionsForError(error){
+    toastr.error("Ocorreu um erro ao processar a sua Solicitação!");
+    this.submittingForm = false;
+
+    if(error.status === 422){
+      this.serverErrorMessages = JSON.parse(error._body).erros;
+    }else{
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, teste mais tarde"];
     }
   }
 
