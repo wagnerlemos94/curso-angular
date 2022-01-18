2b84c145b9005affc502d3c798e8b1f0d1afc134
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2022-01-18 16:51:46 -0300

    Implementa associacao entre entidades

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,8 @@ import { Observable, throwError } from "rxjs";
 import { map, catchError, flatMap } from "rxjs/operators";
 import { Entry } from './entry.model';
 
+import { CategoryService } from '../../categories/shared/category.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +14,7 @@ export class EntryService {
 
   private apiPath: string = "api/entries"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
 
   getAll():Observable<Entry[]>{
@@ -31,19 +33,30 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry>{
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
   }
 
   update(entry: Entry): Observable<Entry>{
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
-    )
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        )
+      })
+    )  
   }
 
   delete(id:number): Observable<any> {
@@ -56,8 +69,6 @@ export class EntryService {
 
 
   private jsonDataToEntries(jsonData: any[]): Entry[]{
-    console.log(jsonData[0] as Entry)
-    console.log(Object.assign(new Entry(), jsonData[0]))
     const entries: Entry[] = [];
     jsonData.forEach(element => {
       const entry = Object.assign(new Entry(), element);
